2063b6421933e5c8609bb1b59f44e5ddf7128c31
Kewal Mishra <kewalmishra555@gmail.com>
2024-07-27 19:04:07 +0000

    ui changes

--- ui.R
+++ ui.R
@@ -1,31 +1,3 @@
-# library(shiny)
-
-# # Defining UI for the application
-# shinyUI(fluidPage(
-#   titlePanel("Titanic Survival Prediction"),
-#   sidebarLayout(
-#     sidebarPanel(
-#       h3("Inputs"),
-#       sliderInput("age", "Age:", min = 0, max = 80, value = 30),
-#       selectInput("sex", "Sex:", choices = c("male", "female")),
-#       selectInput("pclass", "Passenger Class:", choices = c(1, 2, 3)),
-#       sliderInput("sibsp", "Number of Siblings/Spouses Aboard:", min = 0, max = 8, value = 0),
-#       sliderInput("parch", "Number of Parents/Children Aboard:", min = 0, max = 6, value = 0),
-#       numericInput("fare", "Fare:", value = 30, min = 0, step = 1),
-#       actionButton("predict", "Predict")
-#     ),
-#     mainPanel(
-#       h3("Prediction Results"),
-#       textOutput("prediction"),
-#       plotOutput("plot"),
-#       h3("About the Model"),
-#       p("This application uses the Titanic dataset to predict the survival of passengers based on various features. 
-#         Logistic Regression is used as the algorithm to model the probability of survival."),
-#       p("You can adjust the input parameters on the left to see how they affect the prediction. The red dot on the plot represents the current input values.")
-#     )
-#   )
-# ))
-
 library(shiny)
 library(ggplot2)
 
@@ -48,12 +20,7 @@ shinyUI(fluidPage(
     
     mainPanel(
       h3("Prediction Results"),
-      fluidRow(
-        column(12,
-               strong(textOutput("prediction_result")),
-               style = "display: inline-block; vertical-align: middle; margin-left: 10px;"
-        )
-      ),
+      uiOutput("prediction_result"),
       h4("Titanic Survival Prediction"),
       p("The plot below shows the distribution of survival predictions based on the input parameters. The red dot represents the current input values."),
       plotOutput("plot"),
